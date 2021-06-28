831eeb0cb963ab487e47928fbe4d7fbbe20c5f55
yatinmore09 <82366777+yatinmore09@users.noreply.github.com>
2021-06-28 19:11:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,40 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../styles/App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      renderBall: false, //variable needed to be changed
+      renderBall: false,
       posi: 0,
-      ballPosition: { left: "0px" }
+      ballPosition: { left: "0px" },
     };
-    this.renderChoice = this.renderChoice.bind(this);
+    this.renderChoice = this.renderBallOrButton.bind(this);
     this.buttonClickHandler = this.buttonClickHandler.bind(this);
-    this.handleKey = this.handleKey.bind(this);
+    this.keyHandler = this.keyHandler.bind(this);
   }
 
-  //call back function
   buttonClickHandler() {
-    this.setState({ renderBall: true });
+    this.setState((state) => ({
+      renderBall: true,
+    }));
   }
-  renderChoice() {
+
+  renderBallOrButton() {
+    // console.log(this.state.renderBall)
     if (this.state.renderBall) {
       return <div className="ball" style={this.state.ballPosition}></div>;
-    } else
+    } else {
       return (
         <button onClick={this.buttonClickHandler}>Click For One Ball</button>
       );
+    }
   }
 
-  //bind ArrowRight keydown event
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKey);
-  }
-  handleKey(event) {
-    if (event.keyCode === 39) {
+  // bind ArrowRight keydown event
+  keyHandler(e) {
+    if (e.key == "ArrowRight") {
       const left1 = this.state.ballPosition.left;
       let temp = "";
       for (let i = 0; i < left1.length - 2; i++) {
@@ -40,12 +42,16 @@ class App extends Component {
       }
       const val = 5 + Number(temp) + "px";
       this.setState({ ballPosition: { left: val } });
-      this.renderChoice();
+      this.renderBallOrButton();
     }
   }
 
+  componentDidMount() {
+    window.addEventListener("keydown", this.keyHandler);
+  }
+
   render() {
-    return <div className="playground">{this.renderChoice()}</div>;
+    return <div className="playground">{this.renderBallOrButton()}</div>;
   }
 }
 
